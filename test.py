5aaed174f25803a50a218827eabb9ab9608fff16
beto <estadistico1984gmail.com>
2022-01-16 20:00:48 -0300

    cambiamos el rango de 0 a 10 con 20 numeros

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
 ##print('hi world')
 
-x=[1,2,3,4,5,6,7,8,9]
+import math as mt
+
+import numpy as np
+
+#x=[1,2,3,4,5,6,7,8,9,10,11,12]
+
+x = np.linspace(0,10,num=20)
 
 y =[]
 
 for i in range(len(x)):
 
-    w = (x[i]**3-x[i]**2-x[i]-1)/1+x[i]**2 -0.008*x[i]
+    w = mt.sqrt((x[i]**3 + x[i]**2 + x[i] +1)/(1+x[i]**2 - 0.008*x[i]))
+    
+    z = (w)
 
-    y.append(w)
+    y.append(z)
 
 print(y)
-
+print(sum(y))
 
